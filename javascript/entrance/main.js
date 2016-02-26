b56701daf0c9933656e1732b97e8517995becc44
Administrator <237617561@qq.com>
2016-02-26 16:54:36 +0800

    update 2

--- javascript/entrance/main.js
+++ javascript/entrance/main.js
@@ -69,7 +69,7 @@
         keyManager.listenKeyDown(Tack001.proxyKeyFn('keydown')).listenKeyUp(Tack001.proxyKeyFn('keyup'));
         (function () {
             for (var i = 0; i < 10; i++) {
-                Tack001 = new ComputerTack((i * 120 + 120) % 360, 0 , 30, 30, 20).setDistance(3, 3)
+                Tack001 = new ComputerTack((i * 180 + 180) % 540, 0 , 30, 30, 20).setDistance(3, 3)
                     .setImgSrc(baseUrl + 'images/Tack.png').setGame(cGame).setMap(MapArr).setDirection('UPPER');
                 cGame.putGameModel(Tack001);
                 BulletModel.addTack(Tack001);
@@ -88,6 +88,5 @@
             }
         })();
         Game.Run();
-//        setTimeout(Game.stop, 1000);
     });
 })();
